fdc3f613eb642b80186752c7a8862a2c0c8e5104
qazal <qazal.software@gmail.com>
2023-12-29 14:02:23 +0200

    diff cleanup

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,5 @@ pub mod cpu;
 pub mod utils;
 
 fn main() {
-    let value: i32 = -1;
-    let mask = -4;
-    println!("{:b}", value);
-    println!("{:b}", value & mask);
-    println!("{:b}", value as u32);
-    println!("{:b}", (value as u32) & mask as u32);
-    println!("{}", value as u32);
-    println!("{}", (value as u32) & mask as u32);
+    println!("Hello, world!");
 }
