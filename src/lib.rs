9abaf1b291ba9bfe0731889a2fc02a0523bc2082
qazal <77887910+Qazalin@users.noreply.github.com>
2023-12-31 22:18:37 +0200

    interface with tinygrad p1: hipModuleLaunchKernel (#20)

---
+++ src/lib.rs
@@ -0,0 +1,39 @@
+use crate::cpu::CPU;
+use crate::utils::DEBUG;
+use std::os::raw::c_void;
+mod cpu;
+mod state;
+mod utils;
+
+#[no_mangle]
+pub extern "C" fn hipModuleLaunchKernel(
+    grid_dim_x: u32,
+    grid_dim_y: u32,
+    grid_dim_z: u32,
+    block_dim_x: u32,
+    block_dim_y: u32,
+    block_dim_z: u32,
+    _shared_mem_bytes: u32,
+    _stream: *const *const c_void,
+    _kernel_params: *const *const c_void,
+    args_len: u32,
+    args: *const *const c_void,
+) {
+    let mut kernel_args: Vec<u64> = Vec::new();
+    unsafe {
+        for i in 0..args_len {
+            let ptr = *args.offset(i as isize);
+            kernel_args.push(ptr as u64);
+        }
+    }
+
+    if *DEBUG >= 1 {
+        println!(
+            "[remu] launching kernel with global_size {} {} {} local_size {} {} {} {} args {:?}",
+            grid_dim_x, grid_dim_y, grid_dim_z, block_dim_x, block_dim_y, block_dim_z, args_len, kernel_args
+        );
+    }
+
+    let mut cpu = CPU::new();
+    let prg: Vec<u32> = vec![]; // TODO this should come from the lib arg
+}
