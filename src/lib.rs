9adec177780e912a9c8c84aad4903888cffee17c
qazal <qazal.software@gmail.com>
2023-12-31 20:28:35 +0200

    complete interface with python (except lib for now)

--- src/lib.rs
+++ src/lib.rs
@@ -1,18 +1,34 @@
+use crate::utils::DEBUG;
 use std::os::raw::c_void;
 mod cpu;
 mod state;
 mod utils;
 
 #[no_mangle]
-pub extern "C" fn hipModuleLaunchKernel(params: *const *const c_void, len: usize) {
-    let mut kernel_params: Vec<u64> = Vec::new();
-
+pub extern "C" fn hipModuleLaunchKernel(
+    grid_dim_x: u32,
+    grid_dim_y: u32,
+    grid_dim_z: u32,
+    block_dim_x: u32,
+    block_dim_y: u32,
+    block_dim_z: u32,
+    _shared_mem_bytes: u32,
+    _stream: *const *const c_void,
+    _kernel_params: *const *const c_void,
+    args: *const *const c_void,
+) {
+    let mut kernel_args: Vec<u64> = Vec::new();
     unsafe {
-        for i in 0..len {
-            let ptr = *params.offset(i as isize);
-            kernel_params.push(ptr as u64);
+        for i in 0..5 {
+            let ptr = *args.offset(i as isize);
+            kernel_args.push(ptr as u64);
         }
     }
 
-    println!("kernel_params: {:?}", kernel_params);
+    if *DEBUG >= 1 {
+        println!(
+            "[remu] launching kernel with global_size {} {} {} local_size {} {} {} args {:?}",
+            grid_dim_x, grid_dim_y, grid_dim_z, block_dim_x, block_dim_y, block_dim_z, kernel_args
+        );
+    }
 }
