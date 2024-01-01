255537b5ddb9dccd4caaf8a14acd8489f1db04be
qazal <77887910+Qazalin@users.noreply.github.com>
2024-01-01 22:46:11 +0200

    persistant storage for LDS state (#22)

--- src/allocator.rs
+++ src/allocator.rs
@@ -1,13 +1,21 @@
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize)]
 pub struct BumpAllocator {
-    last: u64,
+    pub last: u64,
     pub memory: Vec<u8>,
 }
 
 impl BumpAllocator {
     pub fn new() -> Self {
+        if std::path::Path::new("/tmp/wave.bin").exists() {
+            let enc = std::fs::read("/tmp/wave.bin").unwrap();
+            let decoded: Self = bincode::deserialize(&enc[..]).unwrap();
+            return decoded;
+        }
         Self {
             last: 0,
-            memory: vec![0; 24 * 1_073_741],
+            memory: vec![0; 24 * 1_073],
         }
     }
 
@@ -16,6 +24,12 @@ impl BumpAllocator {
         self.last += size as u64;
         return last_allocated + size as u64;
     }
+
+    /** "persist" memory and locations for the next library call */
+    pub fn save(&self) {
+        let enc = bincode::serialize(&self).unwrap();
+        std::fs::write("/tmp/wave.bin", &enc[..]).unwrap();
+    }
 }
 
 #[cfg(test)]
@@ -23,10 +37,23 @@ mod test {
     use super::*;
     #[test]
     fn test_bump_allocator() {
+        // remove tmp files
+        std::fs::remove_file("/tmp/wave.bin").unwrap_or(());
         let mut allocator = BumpAllocator::new();
         allocator.alloc(4);
         assert_eq!(allocator.last, 4);
         allocator.alloc(9);
         assert_eq!(allocator.last, 13);
+    }
+
+    #[test]
+    fn test_persistant() {
+        // remove tmp files
+        std::fs::remove_file("/tmp/wave.bin").unwrap_or(());
+        let mut allocator = BumpAllocator::new();
+        allocator.alloc(4);
+        allocator.save();
+        let allocator = BumpAllocator::new();
+        assert_eq!(allocator.last, 4);
     }
 }
