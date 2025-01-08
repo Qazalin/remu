549eabd7828bc840907fd95c2dea59327f516847
qazal <qazal.software@gmail.com>
2025-01-08 09:36:16 +0000

    redo rdna3 parsing stuff

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-use crate::utils::{GLOBAL_COUNTER, OSX, PROFILE};
+use crate::utils::{GLOBAL_COUNTER, PROFILE};
 use crate::work_group::WorkGroup;
 use std::os::raw::c_char;
 use std::slice;
@@ -20,25 +20,10 @@ pub extern "C" fn run_asm(
     lz: u32,
     args_ptr: *const u64,
 ) -> i32 {
-    let kernel = match *OSX {
-        true => {
-            let mut lib_bytes: Vec<u8> = Vec::with_capacity(lib_sz as usize);
-            unsafe {
-                lib_bytes.extend((0..lib_sz).map(|i| *lib.offset(i as isize) as u8));
-            }
-            let (kernel, function_name) = utils::read_asm(&lib_bytes);
-            println!(
-                "[remu] launching kernel {function_name} with global_size {gx} {gy} {gz} local_size {lx} {ly} {lz}"
-            );
-            kernel
-        }
-        false => {
-            if lib.is_null() || (lib_sz % 4) != 0 {
-                panic!("Pointer is null or length is not properly aligned to 4 bytes");
-            }
-            unsafe { slice::from_raw_parts(lib as *const u32, (lib_sz / 4) as usize).to_vec() }
-        }
-    };
+    if lib.is_null() || (lib_sz % 4) != 0 {
+        panic!("Pointer is null or length is not properly aligned to 4 bytes");
+    }
+    let kernel = unsafe { slice::from_raw_parts(lib as *const u32, (lib_sz / 4) as usize).to_vec() };
     let dispatch_dim = match (gy != 1, gz != 1) {
         (true, true) => 3,
         (true, false) => 2,
--- src/utils.rs
+++ src/utils.rs
@@ -1,15 +1,12 @@
 use half::f16;
 use lazy_static::lazy_static;
-use std::io::Write;
-use std::process::{Command, Stdio};
 use std::sync::Mutex;
-use std::{env, fs, str};
+use std::{env, str};
 
 pub const END_PRG: u32 = 0xbfb00000;
 lazy_static::lazy_static! {
     pub static ref CI: bool = env::var("CI").map(|v| v == "1").unwrap_or(false);
     pub static ref PROFILE: bool = env::var("PROFILE").map(|v| v == "1").unwrap_or(false);
-    pub static ref OSX: bool = env::var("OSX").map(|v| v == "1").unwrap_or(false);
     pub static ref GLOBAL_DEBUG: bool = env::var("DEBUG").map(|v| v == "1").unwrap_or(false);
 }
 
@@ -21,48 +18,6 @@ pub fn f16_lo(val: u32) -> f16 {
 }
 pub fn f16_hi(val: u32) -> f16 {
     f16::from_bits(((val >> 16) & 0xffff) as u16)
-}
-
-pub fn read_asm(lib: &Vec<u8>) -> (Vec<u32>, String) {
-    if std::env::consts::OS == "macos" {
-        return _read_literal_asm(lib);
-    }
-    let mut child = Command::new("/opt/rocm/llvm/bin/llvm-objdump")
-        .args(&["-d", "-"])
-        .stdin(Stdio::piped())
-        .stdout(Stdio::piped())
-        .spawn()
-        .unwrap();
-    {
-        let stdin = child.stdin.as_mut().unwrap();
-        stdin.write_all(lib).unwrap()
-    }
-    let output = child.wait_with_output().unwrap();
-    let asm = String::from_utf8_lossy(&output.stdout);
-    parse_rdna3(&asm.to_string())
-}
-fn parse_rdna3(content: &str) -> (Vec<u32>, String) {
-    let mut kernel = content.lines().skip(5);
-    let name = kernel
-        .nth(0)
-        .unwrap()
-        .split(" ")
-        .nth(1)
-        .unwrap()
-        .replace(":", "")
-        .replace("<", "")
-        .replace(">", "");
-    let instructions = kernel
-        .map(|line| {
-            line.split_whitespace()
-                .filter(|p| u32::from_str_radix(p, 16).is_ok() && p.len() == 8)
-                .collect::<Vec<&str>>()
-        })
-        .flatten()
-        .map(|x| u32::from_str_radix(x, 16).unwrap())
-        .collect::<Vec<u32>>();
-
-    return (instructions, name.to_string());
 }
 
 pub fn sign_ext(num: u64, bits: usize) -> i64 {
@@ -77,31 +32,6 @@ pub fn sign_ext(num: u64, bits: usize) -> i64 {
 #[cfg(test)]
 mod test {
     use super::*;
-
-    #[test]
-    fn test_parse_rdna3() {
-        let instructions = parse_rdna3(
-            "
-<stdin>:	file format elf64-amdgpu
-
-Disassembly of section .text:
-
-0000000000001600 <E_4>:
-	s_load_b64 s[0:1], s[0:1], null                            // 000000001600: F4040000 F8000000
-	v_dual_mov_b32 v0, 0 :: v_dual_mov_b32 v1, 4               // 000000001608: CA100080 00000084
-",
-        )
-        .0;
-        assert_eq!(instructions.len(), 4);
-        let hexed = instructions
-            .iter()
-            .map(|i| format!("0x{:08x}", i))
-            .collect::<Vec<String>>();
-        assert_eq!(
-            hexed,
-            ["0xf4040000", "0xf8000000", "0xca100080", "0x00000084",]
-        );
-    }
 
     #[test]
     fn test_custom_signed_bits() {
@@ -134,42 +64,11 @@ impl<'a> Colorize for &'a str {
 #[macro_export]
 macro_rules! todo_instr {
     ($x:expr) => {{
-        let instr = format!("{:08X}", $x);
-        use std::env;
-        use std::io::Write;
-        use std::process::{Command, Stdio};
-        let _ = Command::new("pbcopy")
-            .stdin(Stdio::piped())
-            .spawn()
-            .and_then(|mut process| process.stdin.as_mut().unwrap().write_all(instr.as_bytes()));
-        if env::var("OSX").map(|v| v == "1").unwrap_or(false) {
+        if std::env::var("REMU_DEBUG").map(|v| v == "4").unwrap_or(false) {
             panic!("{:08X}", $x)
         }
         Err(1)
     }};
-}
-
-fn _read_literal_asm(lib: &Vec<u8>) -> (Vec<u32>, String) {
-    let asm = String::from_utf8(lib.to_vec()).unwrap();
-    let mut prg = parse_rdna3(&asm);
-    let isolate = env::var("REMU_ISOLATE")
-        .unwrap_or_default()
-        .parse::<i32>()
-        .unwrap_or(0);
-
-    let fp = format!("/tmp/{}.s", prg.1);
-    if isolate == 1 {
-        prg = match std::fs::metadata(&fp) {
-            Ok(_) => parse_rdna3(&fs::read_to_string(fp).unwrap()),
-            Err(_) => {
-                fs::write(fp, asm).unwrap();
-                prg
-            }
-        };
-    } else {
-        fs::write(fp, asm).unwrap();
-    }
-    return prg;
 }
 
 #[derive(Debug)]
